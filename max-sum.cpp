6b08a5574753f35a65782ca8861307815d7999b7
atuldev19 <57002806+atuldev19@users.noreply.github.com>
2019-10-29 19:09:20 +0530

    O(n) technique added

--- max-sum.cpp
+++ max-sum.cpp
@@ -1,4 +1,3 @@
-//to find the maximum sub array through 3 methods of different time complexities.
 #include<bits/stdc++.h>
 using namespace std;
 #define rec(a,i,n) for(int i=a;i<n;i++)
@@ -7,26 +6,13 @@ main()
 int arr[]={-1,2,4,-3,5,2,-5,2};
 int n=*(&arr+1)-arr;
 cout<<"size is "<<n<<endl;
-//O(n^3)
 int best=0;
 int sum=0;
 for(int i=0;i<n;i++)
 {
-for(int j=i;j<n;j++)
-{
-sum=0;
-for(int k=i;k<=j;k++)
-
-{
-sum+=arr[k];
-
-}
+sum=max(arr[i],sum+arr[i]);
 best=max(best,sum);
-}
-
 }
 cout<<"The maximum sum of the elements is "<<best;
 return 0;
-
-
 }
