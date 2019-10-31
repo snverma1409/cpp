7daa2b00994efcd45c6923143db8336671ff2daf
Ankit Sharma <ankitvashisht@hotmail.com>
2019-10-31 09:03:27 +0530

    using divide operator

--- bitmagic.cpp
+++ bitmagic.cpp
@@ -1,12 +1,10 @@
 #include <bits/stdc++.h> 
 using namespace std; 
-int min(int x, int y) 
-{ 
-    return y + ((x - y) & ((x - y) >> (sizeof(int) * CHAR_BIT - 1))); 
-} 
 int smallest(int x, int y, int z) 
 { 
-   return min(x, min(y, z));
+   if (!(y / x))
+        return (!(y / z)) ? y : z; 
+    return (!(x / z)) ? x : z; 
 } 
 int main() 
 { 
